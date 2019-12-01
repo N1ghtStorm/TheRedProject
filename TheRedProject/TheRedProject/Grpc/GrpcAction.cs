9eb966858a531d5bf472b695e48781dfe0d82159
unknown <bushuevdv@mail.ru>
2019-12-01 14:02:02 +0300

    Added grpc client base

---
+++ TheRedProject/TheRedProject/Grpc/GrpcAction.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using GrpcGreeter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheRedProject.Grpc
+{
+    public class GrpcAction : IGrpcAction
+    {
+        public async Task SayHello(string url, string clientName)
+        {
+            // address maybe url = "https://localhost:5001"
+            var channel = GrpcChannel.ForAddress(url);
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = clientName });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TheRedProject/TheRedProject/Grpc/IGrpcAction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheRedProject.Grpc
+{
+    public interface IGrpcAction
+    {
+        Task SayHello(string url, string clientName);
+    }
+}
